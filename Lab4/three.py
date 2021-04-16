249acf8b619204049dadfc2c13a6916e7305cb30
PreranaPandit <preranapandit07@gmail.com>
2021-04-16 09:48:59 +0545

    guessing a number using random

---
+++ Lab4/three.py
@@ -0,0 +1,13 @@
+'''
+3. Write a Python program to guess a number between 1 to 9.
+  Note : User is prompted to enter a guess. If the user guesses wrong then
+  the prompt appears again until the guess is correct, on successful guess,
+  user will get a "Well guessed!" message,
+  and the program will exit.
+'''
+
+import random
+target_num, guess_num = random.randint(1, 10), 0
+while target_num != guess_num:
+    guess_num = int(input('Guess a number between 1 and 10 until you get it right : '))
+print('Well guessed!')
--- Lab4/two.py
+++ Lab4/two.py
@@ -7,3 +7,9 @@ f=float(input("enter the value in fahrenheit degree:"))
 c=(5/9)*(f-32)
 print(c)
 print(f"the value of {f} degree fahrenheit is {c} degree celcius")
+
+
+c=int(input("enter the value in celcius degree:"))
+f=(1.8*c)+32
+print(f)
+print(f"the value of {f} degree celcius is {c} degree fahrenheit")
