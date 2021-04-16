904f05fddaf750edff4e3a9b380c9babc422a6a6
PreranaPandit <preranapandit07@gmail.com>
2021-04-16 09:43:33 +0545

    fahrenheit calculation

---
+++ Lab4/two.py
@@ -0,0 +1,9 @@
+'''
+2. Write a Python program to convert temperatures to and from celsius, fahrenheit.
+  C = (5/9) * (F - 32)
+'''
+
+f=float(input("enter the value in fahrenheit degree:"))
+c=(5/9)*(f-32)
+print(c)
+print(f"the value of {f} degree fahrenheit is {c} degree celcius")
